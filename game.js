c5bccb148a98ccc19c6c2c5b87a4ae5938b31206
Finn Pauls <derfinn@gmail.com>
2018-02-01 19:01:02 +0100

    clean up assets

--- game.js
+++ game.js
@@ -1,5 +1,4 @@
 /*global Phaser*/
-// https://ludumdare-finnp.c9.io/index.html
 
 var game = new Phaser.Game(900, 700, Phaser.AUTO, 'game-div')
 
@@ -8,25 +7,25 @@ var bonus_mod = false
 // mainState
 var mainState = new Phaser.State()
 mainState.preload = function preload() {
-    game.load.image('player', 'assets/parrot.png')
-    game.load.image('bonus', 'assets/boxItem.png')
-    game.load.image('bubble', 'assets/bubble4.png')
-    game.load.image('water', 'assets/waterTop_low.png')
-    game.load.image('coin', 'assets/coinGold.png')
-    game.load.image('hudcoin', 'assets/hudCoin.png')
-    game.load.image('background', 'assets/blue_land.png')
-    game.load.image('plane', 'assets/planeYellow1.png')
-    game.load.image('box', 'assets/boxCoin_boxed.png')
+    game.load.image('player', 'img/parrot.png')
+    game.load.image('bonus', 'img/boxItem.png')
+    game.load.image('bubble', 'img/bubble4.png')
+    game.load.image('water', 'img/waterTop_low.png')
+    game.load.image('coin', 'img/coinGold.png')
+    game.load.image('hudcoin', 'img/hudCoin.png')
+    game.load.image('background', 'img/blue_land.png')
+    game.load.image('plane', 'img/planeYellow1.png')
+    game.load.image('box', 'img/boxCoin_boxed.png')
 }
 
 mainState.create = function create() {
-  
+
   // background
   game.stage.backgroundColor = '#3498db'
   var bg = game.add.tileSprite(0, -30, 1024, 1024, 'background')
   // bg.tint = 0x3498db
-  
-  
+
+
   // score
   this.score = 0
   this.scoreLabel = game.add.text(39, 5, '0', {'fill': '#FFCC00', fontSize: '32px'})
@@ -39,7 +38,7 @@ mainState.create = function create() {
   this.coinGraphic.body.immovable = true
   this.coinGraphic.width = 40
   this.coinGraphic.height = 40
-  
+
   //this.boxes = game.add.physicsGroup()
 
   /*this.plane = game.add.sprite(0,200, 'plane')
@@ -47,8 +46,8 @@ mainState.create = function create() {
   this.plane.anchor.setTo(0.5,0.5)
   this.plane.body.immovable = true
   this.plane.body.velocity.x = 40*/
-  
-  
+
+
 
   // bottom water
   this.water1 = game.add.physicsGroup()
@@ -59,7 +58,7 @@ mainState.create = function create() {
     newWater.body.immovable = true
     newWater.tint = 0xeeeeee
   }
-  
+
   // create player
   this.player = game.add.sprite(350, 350, 'player')
   game.physics.arcade.enable(this.player)
@@ -67,23 +66,23 @@ mainState.create = function create() {
   this.player.anchor.setTo(0.5,0.5)
   this.player.width = 40
   this.player.height = 40
-  
+
   //create bonus group
   if(bonus_mod){
     this.bonuses = game.add.physicsGroup()
   }
 
-  
-    
+
+
   //create bubbles
   this.bubbles = game.add.physicsGroup()
   this.bubbles.setAll('outOfBoundsKill', true)
   this.lastBubble = this.addBubble()
-  
+
   // coin group
   this.coins = game.add.physicsGroup()
-  
-  
+
+
   // top water
   this.water2 = game.add.physicsGroup()
   for(var i = -2; i < 24; i++) {
@@ -91,33 +90,33 @@ mainState.create = function create() {
     newWater.width = 40
     newWater.height = 40
     newWater.body.immovable = true
-  }  
+  }
 
   // add controls
   this.cursor = game.input.keyboard.createCursorKeys()
-  
+
   // physics
   game.physics.startSystem(Phaser.Physics.ARCADE)
-  
+
   // gameover
-  
+
   this.gameoverLabel = game.add.text(this.game.width/2 - 50, this.game.height/2, 'Game Over')
   this.gameoverLabel.visible = false
-  
+
   // countdown
   this.countdownLabel = game.add.text(this.game.width/2 - 21, this.game.height/2 - 100, '3', {fill: '#BC2905', fontSize: '60px'})
   this.countdownLabel.visible = false
-  
-  
+
+
   this.start()
-  
+
   // this.cursor.down.onDown.add(function () {
   //   if(this.gameover) {
   //     this.start()
   //   }
   // }.bind(this))
-  
-  
+
+
   // frames
   this.frame = 0
 }
@@ -172,12 +171,12 @@ mainState.spawnGold = function spawnGold(x, y) {
 }
 
 mainState.update= function update() {
-  
+
   if(this.gameover) this.start()
-  
+
   this.frame++
   if(this.frame > 120) this.frame = 0
-  
+
   /*
   if(this.frame === 60) {
     var newBox = this.boxes.create(this.plane.body.x + 20, this.plane.body.y + 20, 'box')
@@ -185,30 +184,30 @@ mainState.update= function update() {
     newBox.height = 40
     newBox.body.gravity.y = 400
   }*/
-  
+
   this.countdownLabel.visible = this.playerWait > 0
   if(this.playerWait === 170) this.addBubble(this.player.x, 700)
   if(this.playerWait <= 180 - 60) {
     this.countdownLabel.text = '2'
   }
   if(this.playerWait <= 180 - 120) this.countdownLabel.text = '1'
-  
+
   if(this.playerWait === 1) this.score = 0
   if(this.playerWait === 0) {
       this.player.body.gravity.y = 750
   } else {
       this.playerWait--
   }
-  
+
   this.water1.forEach(function (water) {
     water.x += Math.sin(this.frame / 120 * Math.PI * 2)
-  }.bind(this))  
+  }.bind(this))
   this.water2.forEach(function (water) {
     water.x += Math.sin((this.frame + 60) / 120 * Math.PI * 2)
   }.bind(this))
-  
+
   this.scoreLabel.text = this.score.toString()
-  
+
   game.physics.arcade.collide(this.player, this.bubbles, bubbleCollision, null, this)
 
   if(this.player.position.y > 670 && !this.gameover) {
@@ -222,8 +221,8 @@ mainState.update= function update() {
 
   if(!this.lastBubble.alive || this.lastBubble.y < 660)
     this.lastBubble = this.addBubble()
-  
- 
+
+
   if(this.playerWait === 0) {
     if (this.cursor.left.isDown) {
       this.player.body.velocity.x -= (this.player.body.velocity.x + 400) / 15
@@ -233,10 +232,10 @@ mainState.update= function update() {
       this.player.body.velocity.x /= 1.02
     }
   }
-  
+
   // coin score
   game.physics.arcade.collide(this.coinGraphic, this.coins, coinScore, null, this)
-  
+
   if(bonus_mod){
     // bonus
     if(this.bonuses.length < 1){
@@ -248,15 +247,15 @@ mainState.update= function update() {
   game.physics.arcade.collide(this.player, this.bonuses, bonusCollision, null, this)
   }
 
-  
-  
-  
+
+
+
 }
 
 
 game.state.add('main', mainState)
 game.state.start('main')
-  
+
 // functions
 
 function bubbleCollision(player, bubble) {
